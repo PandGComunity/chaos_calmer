1004556f9e4cc8c1fa05318283225b259ececb9f
SSharshunov <s.sharshunov@gmail.com>
2023-02-23 07:10:50 +0300

    Fix build for rt5853f

---
+++ target/linux/ramips/rt305x/profiles/t7838wip-rt5250.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2014 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/T7838WIP-RT5350F
+	NAME:=t7838wip-RT5350F
+	PACKAGES:=\
+		kmod-usb-core kmod-usb-ohci kmod-usb2 \
+		kmod-i2c-core kmod-i2c-ralink \
+		kmod-spi-dev
+endef
+
+define Profile/T7838WIP-RT5350F/Description
+	Package set for t7838wip-RT5350F board
+endef
+
+$(eval $(call Profile,T7838WIP-RT5350F))
